bd584f75843ea59ff45f11b3090a556f3f251fc8
Chris <chris.najera.zambrano@gmail.com>
2021-10-02 22:07:33 +0000

    print of hello resolver

--- schema.py
+++ schema.py
@@ -3,5 +3,19 @@ import graphene
 class Query(graphene.ObjectType):
     hello = graphene.String()
 
-    def resolve_hello(info, kwargs):
+    def resolve_hello(self, info):
         return 'Hello'
+
+schema = graphene.Schema(query=Query)
+
+result = schema.execute(
+    '''
+    {
+        hello
+    }
+    '''
+)
+
+data = dict(result.data.items())
+
+print(data)
